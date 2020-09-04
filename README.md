5c637ba4cb9d293259c718038591e7439f5b58bd
Ruslan Lizogub <rlizogub@kaaiot.io>
2020-09-04 10:41:07 +0300

    4 commit

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # js_course_work
-## How to run script?
+## How to run script with default parameters?
 ```bash
 $ yarn install
 $ yarn run test
+```
+## How to run script with system variables?
+```bash
+$ yarn install
+$ ROLLS=100 DICES=2 DEVIATION=10 yarn test
 ```
--- specs/courseWork.spec.js
+++ specs/courseWork.spec.js
@@ -2,20 +2,19 @@ const superagent = require('superagent');
 require('superagent-retry-delay')(superagent);
 const agent = superagent.agent();
 
-
 describe.only('Test', async function() {
-    it('Get all rolls', async function(numberOfRolls = 1000, dices = 1, deviation = 5) {
+    it('Get all rolls', async function(numberOfRolls = process.env.ROLLS || 1000, dices = process.env.DICES || 1, deviation = process.env.DEVIATION || 5) {
         let res = await agent
             .get(`https://www.random.org/integers/?num=${numberOfRolls}&min=${dices}&max=${dices*6}&col=1&base=10&format=plain&rnd=new`)
             .then((res) => {
                 return res;
-            })
+            });
 
         // Create array based on previous rolls data
-        let allRolls = await res.text.split("\n")
+        let allRolls = await res.text.split("\n");
 
         // Remove the last element from array
-        allRolls.pop()
+        allRolls.pop();
 
         // Create Map with key:value rolls
         let scoreOffRolls = new Map();
@@ -26,16 +25,25 @@ describe.only('Test', async function() {
             else
                 scoreOffRolls[a] = 1;
         });
-        console.log("scoreOffRolls ", scoreOffRolls)
 
         // Get of the ideal percentile
-        let idealPercentile = 100/(dices*5+1)
-        console.log("idealPercentile ", idealPercentile)
+        let idealPercentile = 100/(dices*5+1);
 
         // Get percentiles for each side and add it into the map
         let percentilesForEachSide = new Map();
-        console.log("scoreOffRolls", scoreOffRolls[1])
-        let firsSidePersentile = (scoreOffRolls[1]*100)/numberOfRolls
-        console.log("firsSidePersentile", firsSidePersentile)
+        for (let key in scoreOffRolls){
+            percentilesForEachSide[key] = (scoreOffRolls[key]*100)/numberOfRolls;
+        }
+
+        //Check that deviation is correct
+        let minimumDeviation = idealPercentile - deviation/2;
+        let maximumDeviation = idealPercentile + deviation/2;
+        for (let key in percentilesForEachSide){
+            if(percentilesForEachSide[key] <= maximumDeviation && percentilesForEachSide[key] >= minimumDeviation){
+                console.log(`For value ${key} Result is PASS!`)
+            } else {
+                console.log(`For value ${key} Result is FALSE!`)
+            }
+        }
     });
 });
